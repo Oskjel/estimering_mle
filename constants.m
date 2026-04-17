20570bf41c05a078f163285085a85bce57885cdf
Oskjel <oscarskjelvik@outlook.com>
2026-04-17 17:18:14 +0200

    Clean up

---
+++ constants.m
@@ -0,0 +1,32 @@
+%% Declaration of constants
+% Sampling frequency and period
+F_s = 10^6;
+T = 10^(-6);
+
+% Signal frequency, phase and amplitude
+f_0 = 10^5;
+w_0 = 2*pi*f_0;
+phi = pi/8;
+A = 1;
+
+% SNR_dB to snr
+SNR_dB = [-10 0 10 20 30 40 50 60];
+SNR = 10.^(SNR_dB/10);
+
+% Noise parameters
+mu_w = 0;
+sigma_w = sqrt((0.5 * A^2) ./ SNR);
+
+% CRLB helper constants
+P = N * (N-1) / 2;
+Q = N * (N-1) * (2*N-1) / 6;
+
+% FFT size
+k = [10 12 14 16 18 20];
+M = 2.^k;
+
+% Sampling parameters
+N = 513;
+n0 = -256;
+n = (n0:n0+N-1).'; 
+
