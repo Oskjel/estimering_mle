82bdc2719309765197189007909aae9a262f94c5
benjamhl <benjamhl@stud.ntnu.no>
2026-04-18 09:54:25 +0200

    Working simulation

--- constants.m
+++ constants.m
@@ -6,7 +6,8 @@ T = 10^(-6);
 % Sampling parameters
 N = 513;
 n_0 = -256;
-n = (n_0:n_0+N-1).'; 
+n = (n_0:n_0+N-1).';
+N_realizations = 1000;
 
 % Signal frequency, phase and amplitude
 f_0 = 10^5;
--- main.m
+++ main.m
@@ -2,15 +2,19 @@
 x = complex(zeros(N, length(SNR_dB)));
 w = complex(zeros(N, length(SNR_dB)));
 
-% Generating FFT
+% Allocating space for FFTs
 fft_list = cell(length(SNR_dB), length(M));
 
 % Allocating space for estimators
-w_hat = zeros(length(SNR_dB), length(M));
+w_hat   = zeros(length(SNR_dB), length(M));
 phi_hat = zeros(length(SNR_dB), length(M));
 
-for mc = 1:Nmc
-    % Generating X 
+% Allocating space for estimation errors
+e_w_list   = zeros(N_realizations, length(SNR_dB), length(M));
+e_phi_list = zeros(N_realizations, length(SNR_dB), length(M));
+
+for r = 1:N_realizations
+    % Generating signal 
     for j = 1:length(SNR) % Number of different Signal-to-Noise ratios
         w(:,j) = mu_w + sigma_w(j) * (randn(N,1) + 1i*randn(N,1)) / sqrt(2);
         x(:,j) = A * exp(1i * (w_0*n*T + phi)) + w(:, j);
@@ -30,35 +34,16 @@ for mc = 1:Nmc
             % FFT-based phase estimate
             F_w_hat = 1/N * sum(x(:,j) .* exp(-1i*w_hat(j,k) *n*T));
             phi_hat(j,k) = angle(exp(-1i*w_hat(j,k)*n_0*T) * F_w_hat);
+
+            % Finding estimator error
+            e_w_list(r, j, k)   = w_0 - w_hat(j,k);
+            e_phi_list(r, j, k) = angle(exp(1i * (phi - phi_hat(j,k))));
         end
     end
 end
 
-% Finding estimator error variances
-e_w = w_0 - w_hat;
-e_phi = angle(exp(1i * (phi - phi_hat)));
+% Finding estimator error variance
+var_e_w   = squeeze(var(e_w_list, 0, 1));
+var_e_phi = squeeze(var(e_phi_list, 0, 1));
 
-var_e_w = var(e_w);
-var_e_phi = var(e_phi);
-
-
-% Plot FFT
-j = 6; % pick an SNR index
-
-figure;
-hold on;
-
-for k = 1:length(M)
-    X = fftshift(fft_list{j,k});
-    f = (-M(k)/2:M(k)/2-1)*(F_s/M(k));  % frequency in Hz
-    
-    plot(f, abs(X));
-end
-
-legend("M=" + string(M));
-xlabel('Frequency (Hz)');
-ylabel('|X(f)|');
-title(['FFT for SNR = ' num2str(SNR_dB(j)) ' dB']);
-grid on;
-hold off;
 
